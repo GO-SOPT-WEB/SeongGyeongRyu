a21c4b9c7ba266e249474d3599ef437827613a46
pinktopaz <heather128@ewhain.net>
2023-04-11 16:08:37 +0900

    chore : 메뉴 데이터 추가

---
+++ week1/shopData.js
@@ -0,0 +1,78 @@
+const shopData = [
+  {
+    category: "etc",
+    name: "육회",
+    hashtags: ["승경씨의", "최애음식", "깍둑육회"],
+    imgSrc:
+      "https://d12zq4w4guyljn.cloudfront.net/750_750_20210407072847440_photo_f6dc1796f9a2.jpg",
+  },
+  {
+    category: "etc",
+    name: "스시",
+    hashtags: ["승경씨의", "차애음식"],
+    imgSrc:
+      "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/00/a0000370/img/basic/a0000370_main.jpg?20201002142956&q=80&rw=750&rh=536",
+  },
+  {
+    category: "fruit",
+    name: "체리",
+    hashtags: ["승경씨의", "최애과일"],
+    imgSrc:
+      "https://image-notepet.akamaized.net/resize/620x-/seimage/20200206%2Fea4b9362d0d8759c4084e16e3c624565.jpg",
+  },
+  {
+    category: "fruit",
+    name: "복숭아",
+    hashtags: ["승경씨의", "최애과일2"],
+    imgSrc: "http://www.sjsori.com/news/photo/202212/61429_74531_913.jpg",
+  },
+  {
+    category: "fruit",
+    name: "애플망고",
+    hashtags: ["웨비들", "아프지망고"],
+    imgSrc: "https://sitem.ssgcdn.com/72/45/98/item/1000052984572_i1_1100.jpg",
+  },
+  {
+    category: "etc",
+    name: "양꼬치",
+    hashtags: ["양꼬치엔", "칭다오", "추베릅"],
+    imgSrc:
+      "https://post-phinf.pstatic.net/MjAxOTEyMjBfMTM1/MDAxNTc2ODIzMjM0NDcz.t-nK6RgRUdYRvsTQpxgwAnfOfBPe1N41obXMTQNO2KEg.y4Jy8_-z80JlWhq2gyLbZSRFat27FlMwER9ctWH_lpgg.JPEG/1._%EC%86%A1%ED%99%94%EC%96%91%EA%BC%AC%EC%B9%98.jpg?type=w1200",
+  },
+  {
+    category: "sweet",
+    name: "하리보 젤리",
+    hashtags: ["개발할땐", "젤리가필수"],
+    imgSrc:
+      "https://contents.lotteon.com/itemimage/_v173731/LO/16/68/29/92/36/_1/66/82/99/24/2/LO1668299236_1668299242_1.jpg/dims/optimize/dims/resizemc/400x400",
+  },
+  {
+    category: "sweet",
+    name: "딸기 라떼",
+    hashtags: ["커피말고", "딸기라떼"],
+    imgSrc: "https://m.tlj.co.kr:8008/data/product/2020-12-17_event(2).jpg",
+  },
+  {
+    category: "etc",
+    name: "떡볶이",
+    hashtags: ["주1회", "떡볶이는", "필수"],
+    imgSrc:
+      "https://mp-seoul-image-production-s3.mangoplate.com/786308_1498783561647544.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+  },
+  {
+    category: "sweet",
+    name: "약과",
+    hashtags: ["뒤늦게", "알게된", "약괴매력"],
+    imgSrc:
+      "https://mblogthumb-phinf.pstatic.net/MjAxOTA2MjBfOTgg/MDAxNTYxMDE4OTQ2ODcx.qMiQi6-kz9d9hft74JNZRuJVyIjmRAZKYc0W215eOyEg.7iEztpYfq7qMSsPCuQDVG4MHQnpZzPHO8Q6wME_kNN8g.JPEG.naverschool/GettyImages-1024450056.jpg?type=w800",
+  },
+  {
+    category: "sweet",
+    name: "베이글칩",
+    hashtags: ["옥수수스프맛", "맛도리"],
+    imgSrc:
+      "https://mblogthumb-phinf.pstatic.net/MjAyMjA0MTlfMTk2/MDAxNjUwMzI0NTYyOTQw.cT_l83fc3bytUp0lNGrwTEoYCTJ__P2hh-04_z0dBE4g.cEe8RmlSKqz6yCiUFNwPCwxDSmsn4B1qodWZDmwpjUsg.JPEG.chltkdgns159/1650323328883.jpg?type=w800",
+  },
+];
+
+export { shopData };
