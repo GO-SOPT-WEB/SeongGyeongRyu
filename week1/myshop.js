4ec20309a1af8a512bd5e4fd0089458dcb6c513d
pinktopaz <heather128@ewhain.net>
2023-04-17 21:39:50 +0900

    feat : 전체 해시태그 더보기 기능 구현

--- week1/myshop.js
+++ week1/myshop.js
@@ -1,6 +1,7 @@
 import { shopData } from "./shopData.js";
 
 // 1. 상수 파일에서 데이터 가져와서 보여주기
+const menuWrapper = document.querySelector(".menu__wrapper");
 const menuItems = document.querySelectorAll(".menu__item");
 
 for (let i = 0; i < menuItems.length; i++) {
@@ -15,6 +16,7 @@ for (let i = 0; i < menuItems.length; i++) {
 
   shopData[i].hashtags.forEach((el) => {
     const hashtagItem = document.createElement("li");
+    hashtagItem.className = "menu__hashtag__detail";
     hashtagItem.innerText = el;
     menuHashtags.appendChild(hashtagItem);
   });
@@ -23,23 +25,103 @@ for (let i = 0; i < menuItems.length; i++) {
 }
 
 // 2. 카테고리 필터링 기능
-const categories = document.querySelectorAll("label > button");
-const categoryCheckboxes = categories.forEach((category) => {
+const categories = document.querySelectorAll(".category > li ");
+const categoryTagWrapper = document.querySelector(".category__tag");
+
+categories.forEach((category) => {
   category.addEventListener("click", () => {
-    const filterCategory = category.dataset.filter;
-    console.log(filterCategory);
-    handleRenderFilteredMenus(filterCategory);
+    const categoryTag = document.createElement("div");
+    categoryTag.innerText = category.innerText;
+    categoryTagWrapper.appendChild(categoryTag);
+
+    const categoryCheckbox = category.querySelector("input");
+    categoryCheckbox.checked = !categoryCheckbox.checked;
+
+    selectedCategories.push(category.dataset.filter);
+    handleRenderFilteredMenus(selectedCategories);
   });
 });
 
-const handleRenderFilteredMenus = (filterCategory) => {
-  menuItems.forEach((menu) => {
-    if (filterCategory === "all" || menu.className.includes(filterCategory)) {
-      menu.style.display = "flex";
-    } else {
-      menu.style.display = "none";
-    }
-  });
+const handleRenderFilteredMenus = (selectedCategories) => {
+  if (selectedCategories.length === 0)
+    menuItems.forEach((menu) => (menu.style.display = "flex"));
+  else {
+    menuItems.forEach((menu) => {
+      // 체크한 카테고리에 속한 메뉴만 보여주기
+
+      if (
+        selectedCategories.some((category) => menu.className.includes(category))
+      ) {
+        menu.style.display = "flex";
+      } else {
+        menu.style.display = "none";
+      }
+    });
+  }
+  //   menuItems.forEach((menu) => {
+  //     if(menu.data)
+  //   });
 };
 
 // 3. 카테고리 선택 시 카드섹션 위에 선택한 카테고리 보여주기
+
+// 4. + 클릭시 전체 해시태그 보여주기
+const hashTagWrapperList = document.querySelectorAll(".menu__hashtag");
+let hashTagOpenFlag = true;
+
+hashTagWrapperList.forEach((hashTagWrapper) => {
+  const hashtags = hashTagWrapper.querySelectorAll("ul > li");
+  const moreHashtagBtn = document.querySelectorAll(".menu__hashtag > button");
+
+  const allHashTagWrapper = document.createElement("ul");
+  let hashTagOpenFlag = true;
+
+  //즉, forEach() 메서드의 콜백 함수는 인덱스 0부터 시작하여 모든 요소에 대해 반복 실행되며
+  //클릭 이벤트가 등록될 때의 index 값은 항상 0으로 설정됩니다.
+
+  // for (let i = 0; i < moreHashtagBtn.length; i++) {
+  //   console.log(i);
+  //   moreHashtagBtn[i].addEventListener("click", () => {
+  //     if (hashTagOpenFlag) {
+  //       allHashTagWrapper.className = "menu_allHashtag";
+  //       menuItems[i].prepend(allHashTagWrapper);
+
+  //       shopData[i].hashtags.forEach((el) => {
+  //         const hashtagItem = document.createElement("li");
+  //         hashtagItem.innerText = el;
+  //         allHashTagWrapper.appendChild(hashtagItem);
+  //       });
+  //     } else {
+  //       //removeChild로 캐싱하는 방법 생각해보기
+  //       while (allHashTagWrapper.firstChild) {
+  //         allHashTagWrapper.firstChild.remove();
+  //       }
+  //       allHashTagWrapper.remove();
+  //     }
+  //     hashTagOpenFlag = !hashTagOpenFlag;
+  //   });
+  // }
+
+  for (let i = 0; i < moreHashtagBtn.length; i++) {
+    moreHashtagBtn[i].addEventListener("click", () => {
+      console.log(i);
+      if (hashTagOpenFlag) {
+        allHashTagWrapper.className = "menu_allHashtag";
+        menuItems[i].prepend(allHashTagWrapper);
+
+        shopData[i].hashtags.forEach((el) => {
+          const hashtagItem = document.createElement("li");
+          hashtagItem.innerText = el;
+          allHashTagWrapper.appendChild(hashtagItem);
+        });
+      } else {
+        //removeChild로 캐싱하는 방법 생각해보기
+        while (allHashTagWrapper.firstChild) {
+          allHashTagWrapper.firstChild.remove();
+        }
+        allHashTagWrapper.remove();
+      }
+      hashTagOpenFlag = !hashTagOpenFlag;
+    });
+  }
+});
