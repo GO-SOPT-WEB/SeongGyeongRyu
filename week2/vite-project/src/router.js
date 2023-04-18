26327bcbe7849c13a399c7853ab8a772dc79e69d
pinktopaz <heather128@ewhain.net>
2023-04-18 13:01:57 +0900

    chore : todo 과제 vite-project에 옮기기

--- week2/vite-project/src/router.js
+++ week2/vite-project/src/router.js
@@ -3,7 +3,7 @@ import MyCategory from "./pages/myCategory";
 import Calendar from "./pages/calendar";
 
 export const ROUTE_LIST = [
-  { path: "", element: MainPage },
+  { path: "/", element: MainPage },
   { path: "/calendar", element: Calendar },
   { path: "/my", element: MyCategory },
 ];
