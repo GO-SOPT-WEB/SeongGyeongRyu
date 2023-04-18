93d4c29c34fd58abaf3a8c87cb41251de04c0a74
pinktopaz <heather128@ewhain.net>
2023-04-18 17:25:36 +0900

    style : /my 페이지 UI 구현

--- week2/vite-project/src/pages/myCategory.js
+++ week2/vite-project/src/pages/myCategory.js
@@ -1,13 +1,25 @@
+import { todoInfo } from "../contants/todoInfo";
+
 function MyCategory($container) {
   this.$container = $container;
 
   this.render = () => {
     this.$container.innerHTML = `
-            <main class="myPage">
-                <p>카테고리 페이지야</p>
-            </main>
+            <section class="category__all">
+                
+            </section>
             `;
   };
   this.render();
+
+  const categoryWrapper = document.querySelector(".category__all");
+
+  todoInfo.forEach((el) => {
+    const categoryItem = document.createElement("article");
+    categoryItem.innerText = el.category;
+    categoryItem.className = "category__Item";
+    categoryItem.draggable = true;
+    categoryWrapper.appendChild(categoryItem);
+  });
 }
 export default MyCategory;
