472162aef219cfd6579a7f3d887b324b337b56c4
pinktopaz <heather128@ewhain.net>
2023-04-18 23:53:04 +0900

    feat : 새메뉴 로컬스토리지에 저장

--- week1/myshop.js
+++ week1/myshop.js
@@ -3,6 +3,7 @@ import { shopData } from "./shopData.js";
 // 1. 상수 파일에서 데이터 가져와서 보여주기
 const menuWrapper = document.querySelector(".menu__wrapper");
 const menuItems = document.querySelectorAll(".menu__item");
+const addedItem = JSON.parse(localStorage.getItem("newMenu"));
 
 for (let i = 0; i < menuItems.length; i++) {
   const menuName = menuItems[i].getElementsByTagName("h2")[0];
@@ -67,61 +68,42 @@ const handleRenderFilteredMenus = (selectedCategories) => {
 
 // 4. + 클릭시 전체 해시태그 보여주기
 const hashTagWrapperList = document.querySelectorAll(".menu__hashtag");
+const allHashTagWrapper = document.createElement("ul");
+allHashTagWrapper.className = "menu_allHashtag";
 let hashTagOpenFlag = true;
 
 hashTagWrapperList.forEach((hashTagWrapper) => {
   const hashtags = hashTagWrapper.querySelectorAll("ul > li");
   const moreHashtagBtn = document.querySelectorAll(".menu__hashtag > button");
 
-  const allHashTagWrapper = document.createElement("ul");
-  let hashTagOpenFlag = true;
-
-  //즉, forEach() 메서드의 콜백 함수는 인덱스 0부터 시작하여 모든 요소에 대해 반복 실행되며
-  //클릭 이벤트가 등록될 때의 index 값은 항상 0으로 설정됩니다.
-
-  // for (let i = 0; i < moreHashtagBtn.length; i++) {
-  //   console.log(i);
-  //   moreHashtagBtn[i].addEventListener("click", () => {
-  //     if (hashTagOpenFlag) {
-  //       allHashTagWrapper.className = "menu_allHashtag";
-  //       menuItems[i].prepend(allHashTagWrapper);
-
-  //       shopData[i].hashtags.forEach((el) => {
-  //         const hashtagItem = document.createElement("li");
-  //         hashtagItem.innerText = el;
-  //         allHashTagWrapper.appendChild(hashtagItem);
-  //       });
-  //     } else {
-  //       //removeChild로 캐싱하는 방법 생각해보기
-  //       while (allHashTagWrapper.firstChild) {
-  //         allHashTagWrapper.firstChild.remove();
-  //       }
-  //       allHashTagWrapper.remove();
-  //     }
-  //     hashTagOpenFlag = !hashTagOpenFlag;
-  //   });
-  // }
-
   for (let i = 0; i < moreHashtagBtn.length; i++) {
     moreHashtagBtn[i].addEventListener("click", () => {
-      console.log(i);
-      if (hashTagOpenFlag) {
-        allHashTagWrapper.className = "menu_allHashtag";
-        menuItems[i].prepend(allHashTagWrapper);
-
-        shopData[i].hashtags.forEach((el) => {
-          const hashtagItem = document.createElement("li");
-          hashtagItem.innerText = el;
-          allHashTagWrapper.appendChild(hashtagItem);
-        });
-      } else {
-        //removeChild로 캐싱하는 방법 생각해보기
-        while (allHashTagWrapper.firstChild) {
-          allHashTagWrapper.firstChild.remove();
-        }
-        allHashTagWrapper.remove();
-      }
       hashTagOpenFlag = !hashTagOpenFlag;
     });
   }
 });
+
+if (hashTagOpenFlag) {
+  menuItems[i].prepend(allHashTagWrapper);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.type = "button";
+  deleteBtn.innerText = "❎";
+  deleteBtn.addEventListener(
+    "click",
+    () => (hashTagOpenFlag = !hashTagOpenFlag)
+  );
+  allHashTagWrapper.prepend(deleteBtn);
+
+  shopData[i].hashtags.forEach((el) => {
+    const hashtagItem = document.createElement("li");
+    hashtagItem.innerText = el;
+    allHashTagWrapper.appendChild(hashtagItem);
+  });
+} else {
+  //removeChild로 캐싱하는 방법 생각해보기
+  while (allHashTagWrapper.firstChild) {
+    allHashTagWrapper.firstChild.remove();
+  }
+  allHashTagWrapper.remove();
+}
