1f927665f85e09229db06d0242506f5e6bb623e6
pinktopaz <heather128@ewhain.net>
2023-04-21 21:00:13 +0900

    chore : 전체 해시태그 보여주기 코드 수정

--- week1/shop/myshop.js
+++ week1/shop/myshop.js
@@ -57,7 +57,6 @@ menuWrapper.appendChild(newMenu);
 // 2. 카테고리 필터링 기능
 const categories = document.querySelectorAll(".category > li ");
 const categoryTagWrapper = document.querySelector(".category__tag");
-let isCategoryShowing = Array(4).fill(false);
 
 categories.forEach((category) => {
   category.addEventListener("click", () => {
@@ -80,48 +79,44 @@ categories.forEach((category) => {
       });
 
       categoryTagWrapper.appendChild(newCategoryTag);
-      // selectedCategories.push(category.dataset.filter);
-      renderFilteredMenus(selectedCategories);
     }
   });
 });
 
-const renderFilteredMenus = (selectedCategories) => {
-  if (selectedCategories.length === 0)
-    menuItems.forEach((menu) => (menu.style.display = "flex"));
-  else {
-    menuItems.forEach((menu) => {
-      // 체크한 카테고리에 속한 메뉴만 보여주기
-
-      if (
-        selectedCategories.some((category) => menu.className.includes(category))
-      ) {
-        menu.style.display = "flex";
-      } else {
-        menu.style.display = "none";
-      }
-    });
-  }
-  //   menuItems.forEach((menu) => {
-  //     if(menu.data)
-  //   });
-};
+const renderFilteredMenus = () => {};
 
 // 3. 카테고리 선택 시 카드섹션 위에 선택한 카테고리 보여주기
 
 // 4. + 클릭시 전체 해시태그 보여주기
 const hashTagWrapperList = document.querySelectorAll(".menu__hashtag");
 const allHashTagWrapper = document.createElement("ul");
+const moreHashtagBtn = document.querySelectorAll(".menu__hashtag > button");
 allHashTagWrapper.className = "menu_allHashtag";
-let hashTagOpenFlag = true;
 
-hashTagWrapperList.forEach((hashTagWrapper) => {
-  const hashtags = hashTagWrapper.querySelectorAll("ul > li");
-  const moreHashtagBtn = document.querySelectorAll(".menu__hashtag > button");
+for (let i = 0; i < hashTagWrapperList.length; i++) {
+  const allHashTagWrapper = document.createElement("ul");
+  let hashTagOpenFlag = true;
 
   for (let i = 0; i < moreHashtagBtn.length; i++) {
     moreHashtagBtn[i].addEventListener("click", () => {
       hashTagOpenFlag = !hashTagOpenFlag;
+
+      if (hashTagOpenFlag) {
+        allHashTagWrapper.className = "menu_allHashtag";
+        menuItems[i].prepend(allHashTagWrapper);
+
+        shopData[i].hashtags.forEach((el) => {
+          const hashtagItem = document.createElement("li");
+          hashtagItem.innerText = el;
+          allHashTagWrapper.appendChild(hashtagItem);
+        });
+      } else {
+        //removeChild로 캐싱하는 방법 생각해보기
+        while (allHashTagWrapper.firstChild) {
+          allHashTagWrapper.firstChild.remove();
+        }
+        allHashTagWrapper.remove();
+      }
     });
   }
-});
+}
