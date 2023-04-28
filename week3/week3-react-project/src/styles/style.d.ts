844bd5b79dcb1650c2162762febe8edeb3fadf9b
pinktopaz <heather128@ewhain.net>
2023-04-28 13:03:12 +0900

    inti : Globalstyle 세팅

---
+++ week3/week3-react-project/src/styles/style.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      Pic_Color_Coral: string;
+    };
+  }
+}
---
+++ week3/week3-react-project/src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { css, DefaultTheme } from "styled-components";
+
+const colors = {
+  Pic_Color_Coral: "#FF5D5D",
+};
+
+const theme: DefaultTheme = {
+  colors,
+};
+export default theme;
