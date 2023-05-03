5f3871ca4ad32f85a884e0915658658440bb750b
pinktopaz <heather128@ewhain.net>
2023-05-04 03:58:05 +0900

    style : 버튼 레이아웃 적용

---
+++ week3/week3-react-project/src/component/LevelBtn.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import ButtonLayout from "../Layout/ButtonLayout";
+
+export interface LevelBtnProps {
+  levelName: string;
+  handleLevel: () => void;
+}
+
+const LevelBtn = (props: LevelBtnProps) => {
+  const { levelName, handleLevel } = props;
+  return (
+    <ButtonLayout
+      isLevelBtn={true}
+      buttonText={levelName}
+      handleClick={() => {
+        handleLevel();
+      }}
+    ></ButtonLayout>
+  );
+};
+
+export default LevelBtn;
+
+const StLevelBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 13rem;
+  height: 6rem;
+
+  border: none;
+  border-radius: 2rem;
+  background-color: ${({ theme }) => theme.colors.Matt_Quiz_Btn_Blue};
+
+  box-shadow: 0.2rem 0.2rem 0.2rem 0.2rem gray;
+
+  :hover {
+    background-color: blue;
+  }
+`;
---
+++ week3/week3-react-project/src/component/RestartBtn.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import ButtonLayout from "../Layout/ButtonLayout";
+
+export interface RestartBtnProps {
+  handleRestart: () => void;
+}
+
+const RestartBtn = (props: RestartBtnProps) => {
+  const { handleRestart } = props;
+  return (
+    <ButtonLayout
+      isLevelBtn={false}
+      buttonText={"Restart"}
+      handleClick={() => handleRestart()}
+    ></ButtonLayout>
+  );
+};
+
+export default RestartBtn;
