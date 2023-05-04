2c24695e7753cef677c2a5e9223478f302309c99
pinktopaz <heather128@ewhain.net>
2023-05-05 06:08:04 +0900

    feat : 짝이 맞으면 뒤집은 상태로 유지, 아니면 다시 뒤집기

--- week3/week3-react-project/src/pages/Quiz.tsx
+++ week3/week3-react-project/src/pages/Quiz.tsx
@@ -1,55 +1,129 @@
-import { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
 import LevelBtn from "../component/LevelBtn";
+import ModalPortal from "../component/ModalPortal";
 import QuizCard from "../component/QuizCard";
+import RestartBtn from "../component/RestartBtn";
+import SuccessModal from "../component/SuccessModal";
 import quizList, { quizInfo } from "../constant/quizInfo";
+import { CardContext } from "../contexts/cardContext";
 
 const Quiz = () => {
   const levelList = quizList.map((item: quizInfo) => item.level);
   const [level, setLevel] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
-
-  //카드가 배열되는 순서는 반드시 랜덤으로 지정
-  const randomQuizList = quizList[level].imgList.sort(
-    () => Math.random() - 0.5
+  const [isCardsFlipped, setIsCardsFlipped] = useState<boolean[]>(
+    Array(quizList[level].totalScore).fill(false)
   );
-  console.log(level);
+  const [chosen, setChosen] = useState<number[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  // const handleIsCardOpen = () => {};
+  const handleCloseModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleRandomQuizList = (quizList: quizInfo) => {
+    //중복으로 값이 들어가는 걸 막기 위해 set으로 선언
+    const randomQuiz = new Set();
+    //단계별 정답 수(ex.Easy : 5)에 도달할 때까지 이미지 추가
+    while (randomQuiz.size < quizList.totalScore) {
+      //랜덤으로 추가하기 위해 random 함수 이용
+      randomQuiz.add(
+        quizList.imgList[Math.floor(Math.random() * quizList.imgList.length)]
+      );
+    }
+    //이미지를 두개씩 배열에 다시 넣어서 진짜 문제 배열 만들기
+    const realQuizList: string[] = [];
+    //set이라서 Array로 바꿔주기 위해 Array.from 사용
+    Array.from(randomQuiz).forEach((item) => {
+      realQuizList.push(item as string, item as string);
+    });
+    //이미지 순서 섞어주기
+    realQuizList.sort(() => Math.random() - 0.5);
+
+    return realQuizList;
+  };
+
+  //level이 바뀔 때만 콜백 함수를 실행하고, 그렇지 않은 경우 (ex.clicked가 바뀌는 경우)에는 quizList를 유지한다.
+  const randomQuizList = useMemo(() => {
+    return handleRandomQuizList(quizList[level]);
+  }, [level]);
+
+  //레벨 바뀌면 초기화
+  useEffect(() => {
+    setChosen([]);
+    setIsCardsFlipped([]);
+  }, [level]);
+
+  //setChosen 함수는 비동기적으로 작동하기 때문에
+  //handleCheckPair 함수에서 chosen 배열의 값이 업데이트되기 전의 값을 가지고 실행됨
+  useEffect(() => {
+    if (chosen.length === 2) {
+      setTimeout(() => {
+        handleCheckPair(chosen);
+      }, 1000);
+      setChosen([]);
+    }
+  }, [chosen]);
+
+  useEffect(() => {
+    if (currentScore === quizList[level].totalScore) {
+      setIsModalOpen((prev) => !prev);
+    }
+  }, [currentScore]);
+
+  const handleCheckPair = (chosen: number[]) => {
+    if (randomQuizList[chosen[0]] === randomQuizList[chosen[1]]) {
+      setCurrentScore((prev) => prev + 1);
+    } else {
+      const tempIsCardFlipped = [...isCardsFlipped];
+      tempIsCardFlipped[chosen[0]] = !tempIsCardFlipped[chosen[0]];
+      tempIsCardFlipped[chosen[1]] = !tempIsCardFlipped[chosen[1]];
+      setIsCardsFlipped(tempIsCardFlipped);
+    }
+  };
 
   return (
     <>
-      <StHeader>
-        🦊 매튜를 맞춰보아요 🦊
-        <p>
-          현재 스코어 : {currentScore}/{quizList[level].totalScore}
-        </p>
-      </StHeader>
+      <CardContext.Provider value={{ isCardsFlipped, setIsCardsFlipped }}>
+        <StHeader>
+          <RestartBtn handleRestart={() => window.location.reload()} />
+          🦊 매튜를 맞춰보아요 🦊
+          <p>
+            현재 스코어 : {currentScore}/{quizList[level].totalScore}
+          </p>
+        </StHeader>
 
-      <StQuizWrapper>
-        <nav>
-          {levelList.map((level, idx) => (
-            <LevelBtn
-              key={idx}
-              levelName={level}
-              onClick={() => {
-                setLevel(idx);
-              }}
-            />
-          ))}
-        </nav>
+        <StQuizWrapper>
+          <nav>
+            {levelList.map((level, idx) => (
+              <LevelBtn
+                key={idx}
+                levelName={level}
+                handleLevel={() => {
+                  setLevel(idx);
+                }}
+              />
+            ))}
+          </nav>
 
-        <section>
-          {randomQuizList.map((item, idx) => (
-            <QuizCard
-              key={idx}
-              isOpen={true}
-              quizImgSrc={item}
-              // onClick={() => handleIsCardOpen()}
-            />
-          ))}
-        </section>
-      </StQuizWrapper>
+          <section>
+            {randomQuizList.map((item, idx) => (
+              <QuizCard
+                key={idx}
+                quizImgSrc={item}
+                order={idx}
+                handleClick={() => setChosen((prev) => [...prev, idx])}
+              />
+            ))}
+          </section>
+        </StQuizWrapper>
+        {isModalOpen && (
+          <ModalPortal>
+            <SuccessModal level={level} handleClick={handleCloseModal} />
+          </ModalPortal>
+        )}
+      </CardContext.Provider>
     </>
   );
 };
@@ -78,7 +152,6 @@ const StHeader = styled.header`
 `;
 
 const StQuizWrapper = styled.div`
-  width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -93,8 +166,9 @@ const StQuizWrapper = styled.div`
   > section {
     display: flex;
     flex-wrap: wrap;
+    justify-content: center;
 
-    width: 80%;
+    width: 85%;
 
     gap: 3rem;
   }
