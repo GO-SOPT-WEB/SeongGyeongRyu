430f344b640c31b748953a398382c1c45af4d20e
pinktopaz <heather128@ewhain.net>
2023-05-10 14:24:49 +0900

    style : globalStyle 세팅

---
+++ week4/week4-project/src/style/styled.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      Weather_Card_Blue: string;
+      Weather_Header_Blue: string;
+      Weather_Text_White: string;
+    };
+  }
+}
---
+++ week4/week4-project/src/style/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  Weather_Card_Blue: "#91D8E4",
+  Weather_Header_Blue: "#82AA23",
+  Weather_Text_White: "#ffffff",
+};
+
+const theme: DefaultTheme = {
+  colors,
+};
+
+export default theme;
