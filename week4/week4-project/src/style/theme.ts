0a368af5651cfd7bfbcb6ac4a6abcec86690fd0c
pinktopaz <heather128@ewhain.net>
2023-05-10 14:33:50 +0900

    style : 헤더 색상 수정

--- week4/week4-project/src/style/theme.ts
+++ week4/week4-project/src/style/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "styled-components";
 
 const colors = {
   Weather_Card_Blue: "#91D8E4",
-  Weather_Header_Blue: "#82AA23",
+  Weather_Header_Blue: "#82AAE3",
   Weather_Text_White: "#ffffff",
 };
 
